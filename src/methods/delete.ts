c562503275813ab1f87da570f9669d6349a414f4
Mulwor <adigezalli@yandex.ru>
2026-01-05 18:14:49 +0100

    refactor: code added try, catch in delete and post methods

--- src/methods/delete.ts
+++ src/methods/delete.ts
@@ -5,24 +5,21 @@ import { sendResponse } from "../utils/sendResponse.js";
 import { removeUser } from "../database/removeUser.js";
 
 export const removeRequest = async (response: ServerResponse, url: string) => {
-  const id = url.split('/').pop();
-
-  if (!id || !validate(id)) {
-    sendResponse(response, 400, { message: 'Invalid user ID' })
+  try {
+    const id = url.split('/').pop();
+      
+    if (!id || !validate(id)) {
+      sendResponse(response, 400, { message: 'Invalid user ID' })
+    }
+        
+    const user = users.find((user) => user.id === id);
+      if (!user) {
+        return sendResponse(response, 404, { message: 'User does not exist. Try find another user' });
+      }
+        
+      await removeUser(user.id);
+      return sendResponse(response, 204, {});
+    } catch {
+      sendResponse(response, 500, { message: "Server error" })
   }
-
-  const user = users.find((user) => user.id === id);
-  if (!user) {
-    return sendResponse(response, 404, { message: 'User does not exist. Try find another user' });
-  }
-
-  await removeUser(user.id);
-  return sendResponse(response, 204, {});
 }
-
-
-// - **DELETE** `api/users/{userId}` используется для удаления существующего пользователя из базы данных
-
-// - Сервер должен отвечать со status code 204, если запись найдена и удалена
-// - Сервер должен отвечать со status code 400 и соответствующим сообщением, если userId невалиден (не uuid)
-// - Сервер должен отвечать со status code 404 и соответствующим сообщением, если записи с id === userId не существует
--- src/methods/post.ts
+++ src/methods/post.ts
@@ -7,16 +7,20 @@ export const postRequest = async (request: IncomingMessage, response: ServerResp
   let body = ""
   request.on('data', chunk => ( body += chunk ));
   request.on('end', async () => {
-    const data = JSON.parse(body);
-    const { username, age, hobbies } = data;
-
-    if (!username || typeof age !== 'number' || !Array.isArray(hobbies)) {
-      sendResponse(response, 400, { message: 'Missing or invalid arguments: username: string, age: number, hobbies: Array<string>' });
-      return;
+    try {
+      const data = JSON.parse(body);
+      const { username, age, hobbies } = data;
+        
+      if (!username || typeof age !== 'number' || !Array.isArray(hobbies)) {
+        sendResponse(response, 400, { message: 'Missing or invalid arguments: username: string, age: number, hobbies: Array<string>' });
+        return;
+      }
+        
+      const newUser = { id: v4(), username, age, hobbies };
+      await users.push(newUser);
+      sendResponse(response, 201, newUser);
+    } catch {
+      sendResponse(response, 500, { message: "Server error" })
     }
-
-    const newUser = { id: v4(), username, age, hobbies };
-    await users.push(newUser);
-    sendResponse(response, 201, newUser);
   });
 }
